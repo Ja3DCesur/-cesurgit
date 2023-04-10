a4ed1fe0c5f9d9ac0d46f4ed128b0bae4de73bc6
josea <josea@DESKTOP-L0F4IDI>
2023-04-10 18:34:51 +0200

    primera modificacion

---
+++ cesurgit/src/cesurgit/HolaGit.java
@@ -0,0 +1,17 @@
+package cesurgit;
+
+public class HolaGit {
+
+	public static void main(String[] args) {
+		
+        System.out.println("Hola Git");
+		
+		System.out.println("Hola Git 1");
+		
+		System.out.println("Hola Git 2");
+		
+		System.out.println("Hola Git 3");
+
+	}
+
+}
